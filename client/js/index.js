c5a64787a5dcddf7266163008849e68b28fa2a42
Ivan Trujillo <krival1987@gmail.com>
2018-02-19 11:56:05 -0500

    set up external index.js file to serve up socket. Tested triggered communication between server and client using html button

--- client/js/index.js
+++ client/js/index.js
@@ -1,4 +1,8 @@
 const socket = io();
-socket.emit('happy', {
- reason:'it\'s my birfday'
-})
+
+const happy = async() =>{
+socket.emit('happy',{ reason: 'it\'s my birdday'})
+}
+socket.on('serverMsg', (async(data) =>{
+console.log(data.msg)
+}))
--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const http = require('http').Server(app)
 const socket = require('socket.io')(http)
 const PORT = process.env.PORT || 8080;
 
+app.use(express.static(__dirname + '/client'))
 app.get('/', (async(req, res)=>{
  res.sendFile(__dirname + '/client/html/index.html')
 }))
-app.use('/client', express.static(__dirname + '/client/html'))
 http.listen(PORT, (async () =>{
  console.log(`Listening on ${PORT}`)
 }))
@@ -16,6 +16,9 @@ socket.on('connection', (async(sockets)=>{
  console.log('a user is connected')
  sockets.on('happy', (async(data) =>{
   console.log('happy ', data.reason)
+  if(!!data.reason === true){
+   sockets.emit('serverMsg', { msg: 'howdy' })
+  }
  }))
  sockets.emit('serverMsg', { msg: "hello "})
  sockets.on("disconnect", (async()=>{
