dc4290c2abeadbab65a1b5896438dbc76cf5637f
Ivan Trujillo <krival1987@gmail.com>
2018-02-19 13:30:13 -0500

    added multiplayer sorting function and timer, added canvas to verify the position of the players

--- client/js/index.js
+++ client/js/index.js
@@ -15,8 +15,20 @@ console.log(data.msg)
 }))
 // newPosition > this socket connection will take the new x and y position of the element, by clearing the canvas and redrawging.
 
-socket.on('newPosition', (async(data)=>{
- ctx.clearRect(0,0,250,250);
- ctx.fillText("I", data.x,data.y)
-}))
+// socket.on('newPosition', (async(data)=>{
+//  ctx.clearRect(0,0,250,250);
+//  ctx.fillText("I", data.x,data.y)
+// }))
 // new position function works
+socket.on('newPositions', (async(data)=>{
+
+ ctx.clearRect(0,0,250,250);
+ // data.map((position)=>{
+ //  ctx.fillText("S", position.x, position.y)
+ // })
+ for (var i = 0; i < data.length; i++) {
+  ctx.fillText(data[i].number, data[i].x, data[i].y)
+ }
+}))
+
+// the newpositions function is an array this time, therefore we need to loop through the array when handling multiple users
--- server.js
+++ server.js
@@ -16,9 +16,10 @@ const SOCKET_LIST = {}
 
 socket.on('connection', (async(sockets)=>{
  console.log('a user is connected')
- socket.id = await crypto.randomBytes(16).toString("hex")
+ socket.id = Math.random()
  socket.x=0;
  socket.y=0;
+ socket.number = "" + Math.floor(10 * Math.random())
  SOCKET_LIST[socket.id] = sockets
  sockets.on('happy', (async(data) =>{
   console.log('happy ', data.reason)
@@ -31,14 +32,15 @@ socket.on('connection', (async(sockets)=>{
   console.log('user has disconnected')
  }))
 }))
-setInterval(()=>{
- for(socket_user in SOCKET_LIST){
-  const user = SOCKET_LIST[socket_user]
-   socket.x++;
-   socket.y++;
-   socket.emit('newPosition', { x:socket.x, y:socket.y})
- }
-},1000/25)
+// Handling One PLayer
+// setInterval(()=>{
+//  for(socket_user in SOCKET_LIST){
+//   const user = SOCKET_LIST[socket_user]
+//    socket.x++;
+//    socket.y++;
+//    socket.emit('newPosition', { x:socket.x, y:socket.y})
+//  }
+// },1000/25)
 /*
 
              We will have two types of communication using the Express module the client will be asking the server for a file such as the sprite.
@@ -50,4 +52,20 @@ setInterval(()=>{
             We want to use a socket list to handle multiple users the list will have an ID and to generate the id we will use the Crypto library.
 
             We add the first player: To do that we will set an interval function that runs every 250ms, this function will loop through the player and it will retrieve the players position, then the position will be shown to the user
+
+           For multiplayer we will refine the setInterval function slightly by adding a package content array that will store the other user positions, then we will loop through the list again and emit the positions therefore sharing with multiple users.
 */
+setInterval(()=> {
+ const pack = []
+ for (player in SOCKET_LIST) {
+  const players = SOCKET_LIST[player]
+  socket.x++
+  socket.y++
+
+  pack.push({ x: socket.x, y: socket.y, number: socket.number })
+ }
+ for (viewer in SOCKET_LIST) {
+  const players = SOCKET_LIST[viewer]
+  socket.emit('newPositions', pack)
+ }
+}, 10000/25);
